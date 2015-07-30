1c6b948cbc4a20251339fac00fab8063fac4cc6f
NickMaster <master-7@inbox.ru>
2015-07-30 15:19:31 +0300

    init

---
+++ PassWordGenerator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace master7\pass;
+
+/**
+ * Class PassWordGenerator
+ * @package master7\pass
+ */
+class PassWordGenerator {
+
+    /**
+     * Generate the password
+     * @param int $countWord
+     * @param int $countNumber
+     * @return null|string
+     */
+    public static function generate($countWord = 1, $countNumber = 3)
+    {
+        $password = null;
+
+        $dictionary = require('dictionary.php');
+
+        for($count = 1; $count <= $countWord; $count++)
+        {
+            $password .= $dictionary[rand(0, count($dictionary))];
+        }
+
+        for($count = 1; $count <= $countNumber; $count++)
+        {
+            $password .= rand(0, 9);
+        }
+        return $password;
+    }
+
+}
